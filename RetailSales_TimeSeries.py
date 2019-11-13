452310892d1777c0c96238cbdff20b33cf11cc0b
sandhyaraniMukkamala <41523347+sandhyaraniMukkamala@users.noreply.github.com>
2019-11-13 14:29:34 -0500

    Add files via upload

---
+++ RetailSales_TimeSeries.py
@@ -0,0 +1,240 @@
+import warnings
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+plt.style.use('fivethirtyeight')
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib
+import xlrd
+from pylab import rcParams
+
+# Read excel data...
+df = pd.read_excel(r'C:/Users/sandhya.mukkamala/Downloads/Superstore.xls')
+# Perform time series analysis and forecasting for furniture sales
+furniture = df.loc[df['Category'] == 'Furniture']
+# Check time series of data i.e start and end of data
+furniture['Order Date'].min()
+furniture['Order Date'].max()
+# It seems there is 4year data present for 'Furniture' sales
+
+# Data Pre-processing
+# Remove un wanted columns, as we only need to check sales made on different days we only need order date and sales($)
+cols = ['Row ID', 'Order ID', 'Ship Date', 'Ship Mode', 'Customer ID', 'Customer Name', 'Segment', 'Country', 'City',
+        'State', 'Postal Code', 'Region', 'Product ID', 'Category', 'Sub-Category', 'Product Name', 'Quantity',
+        'Discount', 'Profit']
+furniture.drop(cols, axis=1, inplace=True)
+# Order sales by order date
+furniture = furniture.sort_values('Order Date')
+# Check for missing values
+furniture.isnull().sum()
+# No missing values found for both columns
+
+# Indexing the time series data
+furniture = furniture.groupby('Order Date')['Sales'].sum().reset_index()
+furniture = furniture.set_index('Order Date')
+# As there is too much data to work with, average daily sales values for month can be used and start date of each month
+# can be used as timestamp.
+y = furniture['Sales'].resample('MS').mean()
+y['2017':]
+# Visualizing furniture sales time series data
+y.plot(figsize=(15, 6))
+plt.show()
+# Data has seasonality pattern, such as sales are always low at the beginning of the year  and high at the end of year.
+# Also, there is always a upward trend within a single year with a couple of low months in mid of year
+
+# Visualize data using time series decomposition
+rcParams['figure.figsize'] = 18, 8
+decomposition = sm.tsa.seasonal_decompose(y, model='additive')
+fig = decomposition.plot()
+plt.show()
+# It clearly shows that data is unstable and there is seasonality present in the data
+
+# Time Series forecasting with ARIMA
+# Parameter selection for ARIMA Time Series model
+p = d = q = range(0, 2)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+
+print('Examples of parameter combinations for Seasonal ARIMA...')
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
+print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
+print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
+
+for param in pdq:
+    for param_seasonal in seasonal_pdq:
+        try:
+            mod = sm.tsa.statespace.SARIMAX(y,
+                                            order=param,
+                                            seasonal_order=param_seasonal,
+                                            enforce_stationarity=False,
+                                            enforce_invertibility=False)
+
+            results = mod.fit()
+
+            print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+        except:
+            continue
+
+mod = sm.tsa.statespace.SARIMAX(y,
+                                order=(1, 1, 1),
+                                seasonal_order=(1, 1, 0, 12),
+                                enforce_stationarity=False,
+                                enforce_invertibility=False)
+
+results = mod.fit()
+
+print(results.summary().tables[1])
+
+results.plot_diagnostics(figsize=(16, 8))
+plt.show()
+plt.close()
+
+# Validating Forecasts
+# Compare predicted sales to real sales to validate the forecast
+# setting the forecasts to start from 2017-07-01
+
+pred = results.get_prediction(start=pd.to_datetime('2017-01-01'), dynamic=False)
+pred_ci = pred.conf_int()
+
+ax = y['2014':].plot(label='observed')
+pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7, figsize=(14, 7))
+
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.2)
+
+ax.set_xlabel('Date')
+ax.set_ylabel('Furniture Sales')
+plt.legend()
+
+plt.show()
+
+# Overall forecast values align with original values
+y_forecasted = pred.predicted_mean
+y_truth = y['2017-01-01':]
+
+# Compute the mean square error
+mse = ((y_forecasted - y_truth) ** 2).mean()
+print('The Mean Squared Error of our forecasts is {}'.format(round(mse, 2)))
+
+# Producing and visualizing forecasts
+pred_uc = results.get_forecast(steps=100)
+pred_ci = pred_uc.conf_int()
+
+ax = y.plot(label='observed', figsize=(14, 7))
+pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.25)
+ax.set_xlabel('Date')
+ax.set_ylabel('Furniture Sales')
+
+plt.legend()
+plt.show()
+# This model clearly shows seasonality and we can forecast further but it is never ending. Forecast other products and
+# compare
+# Time Series comparison furniture sales and Office Supplies
+# Data Pre-processing
+furniture = df.loc[df['Category'] == 'Furniture']
+office = df.loc[df['Category'] == 'Office Supplies']
+furniture.shape, office.shape
+cols = ['Row ID', 'Order ID', 'Ship Date', 'Ship Mode', 'Customer ID', 'Customer Name', 'Segment', 'Country', 'City',
+        'State', 'Postal Code', 'Region', 'Product ID', 'Category', 'Sub-Category', 'Product Name', 'Quantity',
+        'Discount', 'Profit']
+furniture.drop(cols, axis=1, inplace=True)
+office.drop(cols, axis=1, inplace=True)
+
+furniture = furniture.sort_values('Order Date')
+office = office.sort_values('Order Date')
+
+furniture = furniture.groupby('Order Date')['Sales'].sum().reset_index()
+office = office.groupby('Order Date')['Sales'].sum().reset_index()
+furniture.head()
+office.head()
+
+# Data Exploration
+furniture = furniture.set_index('Order Date')
+office = office.set_index('Order Date')
+
+y_furniture = furniture['Sales'].resample('MS').mean()
+y_office = office['Sales'].resample('MS').mean()
+
+furniture = pd.DataFrame({'Order Date':y_furniture.index, 'Sales':y_furniture.values})
+office = pd.DataFrame({'Order Date': y_office.index, 'Sales': y_office.values})
+
+store = furniture.merge(office, how='inner', on='Order Date')
+store.rename(columns={'Sales_x': 'furniture_sales', 'Sales_y': 'office_sales'}, inplace=True)
+store.head()
+# It shows that furniture and office sales has same seasonality, also early of the year is off season for both
+# categories and sometimes office supplies crossed average daily sales of furniture sales
+# When is the first time office supplies surpassed furniture sales
+first_date = store.ix[np.min(list(np.where(store['office_sales'] > store['furniture_sales'])[0])), 'Order Date']
+
+print("Office supplies first time produced higher sales than furniture is {}.".format(first_date.date()))
+
+# Time Series modelling with Prophet
+from fbprophet import Prophet
+furniture = furniture.rename(columns={'Order Date': 'ds', 'Sales': 'y'})
+furniture_model = Prophet(interval_width=0.95)
+furniture_model.fit(furniture)
+
+office = office.rename(columns={'Order Date': 'ds', 'Sales': 'y'})
+office_model = Prophet(interval_width=0.95)
+office_model.fit(office)
+
+furniture_forecast = furniture_model.make_future_dataframe(periods=36, freq='MS')
+furniture_forecast = furniture_model.predict(furniture_forecast)
+
+office_forecast = office_model.make_future_dataframe(periods=36, freq='MS')
+office_forecast = office_model.predict(office_forecast)
+
+plt.figure(figsize=(18, 6))
+furniture_model.plot(furniture_forecast, xlabel = 'Date', ylabel = 'Sales')
+plt.title('Furniture Sales');
+
+plt.figure(figsize=(18, 6))
+office_model.plot(office_forecast, xlabel = 'Date', ylabel = 'Sales')
+plt.title('Office Supplies Sales');
+
+# Compare Forecasts
+# As we have forecasts for three years for those two categories into the future, join them togeather to compare their
+# future forecasts
+furniture_names = ['furniture_%s' % column for column in furniture_forecast.columns]
+office_names = ['office_%s' % column for column in office_forecast.columns]
+
+merge_furniture_forecast = furniture_forecast.copy()
+merge_office_forecast = office_forecast.copy()
+
+merge_furniture_forecast.columns = furniture_names
+merge_office_forecast.columns = office_names
+
+forecast = pd.merge(merge_furniture_forecast, merge_office_forecast, left_on='furniture_ds',
+                    right_on='office_ds', how='inner')
+
+forecast = forecast.rename(columns={'furniture_ds': 'Date'}).drop('office_ds', axis=1)
+forecast.head()
+
+# Visualize the trend and forecast
+plt.figure(figsize=(10, 7))
+plt.plot(forecast['Date'], forecast['furniture_trend'], 'b-')
+plt.plot(forecast['Date'], forecast['office_trend'], 'r-')
+plt.legend(); plt.xlabel('Date'); plt.ylabel('Sales')
+plt.title('Furniture vs. Office Supplies Sales Trend');
+
+
+plt.figure(figsize=(10, 7))
+plt.plot(forecast['Date'], forecast['furniture_yhat'], 'b-')
+plt.plot(forecast['Date'], forecast['office_yhat'], 'r-')
+plt.legend(); plt.xlabel('Date'); plt.ylabel('Sales')
+plt.title('Furniture vs. Office Supplies Estimate');
+
+# Trends and Patterns
+furniture_model.plot_components(furniture_forecast);
+
+# It is good to see both furniture and office supplies sales are increasing gradually over time and office supplies
+# is slightly higher than furniture sales.
+# The worst month for furniture is April, the worst month for office supplies is February.
+# The best month for furniture is December and the best month for office supplies is November.
